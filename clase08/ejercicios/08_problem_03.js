2c6169d270bc61b7635f1ecc158663e2eab83148
christian-huelmo <christian.huelmo@hikko.is>
2022-08-09 13:57:54 -0300

    Agregada la letra al ejercicio 3

--- clase08/ejercicios/08_problem_03.js
+++ clase08/ejercicios/08_problem_03.js
@@ -1,3 +1,10 @@
+/*
+Crear un servidor que permita elegir y subir un archivo utilizando un formulario servido desde su espacio público.
+Dicho archivo se almacenará en una carpeta propia del servidor llamada 'uploads'.
+El nombre del archivo guardado se formará con el nombre original anteponiéndole un timestamp (Date.now()) seguido con un guión. Ej: 1610894554093-clase1.zip
+Utilizar express y multer en un proyecto de servidor que escuche en el puerto 8080.
+
+*/
 const express = require('express');
 const multer = require('multer');
 
